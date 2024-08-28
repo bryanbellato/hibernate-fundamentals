84c365a0397b914fd4be6951386a00d32add298b
bryanbellato <brbhasegawa@gmail.com>
2024-08-28 08:00:05 -0300

    Project initialized. Imported PostgreSQL driver, JUnit 4.13, Hibernate ORM and Hibernate Persistance. Entities Course and Enrollment created.

---
+++ src/main/java/br/com/bellato/domain/Course.java
@@ -0,0 +1,54 @@
+package br.com.bellato.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TB_COURSE")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_seq")
+    @SequenceGenerator(name = "course_seq", sequenceName = "sq_course", initialValue = 1, allocationSize = 1)
+    private long id;
+
+    @Column(name = "CODE", length = 10, nullable = false, unique = true)
+    private String code;
+
+    @Column(name = "NAME", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "DESCRIPTION", length = 100, nullable = false)
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/br/com/bellato/domain/Enrollment.java
@@ -0,0 +1,66 @@
+package br.com.bellato.domain;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "TB_ENROLLMENT")
+public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "enroll_seq")
+    @SequenceGenerator(name = "enroll_seq", sequenceName = "sq_enroll", initialValue = 1, allocationSize = 1)
+    private long id;
+
+    @Column(name = "CODE", length = 10, nullable = false, unique = true)
+    private String code;
+
+    @Column(name = "DATE_ENROLLMENT", nullable = false)
+    private Instant dateEnrollment;
+
+    @Column(name = "VALUE", nullable = false)
+    private Double value;
+
+    @Column(name = "STATUS", nullable = false)
+    private String status;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Instant getDateEnrollment() {
+        return dateEnrollment;
+    }
+
+    public void setDateEnrollment(Instant dateEnrollment) {
+        this.dateEnrollment = dateEnrollment;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
